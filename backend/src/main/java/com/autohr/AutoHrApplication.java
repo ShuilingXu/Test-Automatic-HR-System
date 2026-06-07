72f42edec35ed42f0dbed1fccfceb2ccf4f15f26
Takamatsu Tomori <xxy051122@qq.com>
2026-06-07 20:08:02 +0800

    初始化 文件结构和配置文件
    初始化 文件结构和配置文件

---
+++ backend/src/main/java/com/autohr/AutoHrApplication.java
@@ -0,0 +1,11 @@
+package com.autohr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoHrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoHrApplication.class, args);
+    }
+}
